13ee46251a57c3c973611f2c3de73676b4583cb7
Troy Neubauer <troyneubauer@gmail.com>
2021-04-19 15:47:42 -0400

    Fixed ui gap issues between menu bar and board

--- src/main/java/com/troy/chess/Main.java
+++ src/main/java/com/troy/chess/Main.java
@@ -1,14 +1,12 @@
 package com.troy.chess;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.ToolBar;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -19,22 +17,23 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class Main extends Application {
-    private BorderPane board = new BorderPane();
-    private Pane pane = new Pane();
+    private Pane board = new Pane();
+    private VBox root = new VBox();
+    private MenuBar mainMenu = new MenuBar();
 
     private int boardSize;
     private int osTitleBarSize;
+    private int mainMenuSize;
 
     private ImageView[] pieces;
 
     private Image KING, ELEPHANT;
 
     /**
-     * The index of the last square that was clicked or -1 in no square has been clicked yet.
-     * Used for storing the first square clicked when making a move
+     * The index of the last square that was clicked or -1 in no square has been
+     * clicked yet. Used for storing the first square clicked when making a move
      */
     private int lastClickedIndex = -1;
-
 
     private static double squareX(int size, int file, double squarePX) {
         return squarePX * file;
@@ -42,7 +41,9 @@ public class Main extends Application {
 
     private static double squareY(int size, int rank, double squarePX) {
         double boardHeightPX = size * squarePX;
-        return boardHeightPX - rank * squarePX;
+        return boardHeightPX - (rank + 1) * squarePX;
+        // (rank + 1) here because rank ranges from 0..(this.boardSize-1) and we want
+        // the top rank to be at y=0
     }
 
     private double lastSquarePX = 10;
@@ -51,23 +52,26 @@ public class Main extends Application {
      * Resizes the existing squares in java fx so that they each have the requested
      * size in pixels
      * 
-     * @param squarePX How wide and tall in pixels each square should be. Use -1 to use the last value
+     * @param squarePX How wide and tall in pixels each square should be. Use -1 to
+     *                 use the last value
      */
     private void resizeBoard(double squarePX) {
         if (squarePX == -1) {
             squarePX = this.lastSquarePX;
-            System.out.println("Using last square px" + squarePX);
+            // System.out.println("Using last square px" + squarePX);
         } else {
             this.lastSquarePX = squarePX;
         }
-        double sideLen = Math.sqrt(this.board.getChildren().size());
 
         for (int ii = 0; ii < this.board.getChildren().size(); ii++) {
             final int i = ii;
+            Node node = board.getChildren().get(i);
+            if (!(node instanceof Rectangle))
+                break;
 
             int rank = i / this.boardSize;
             int file = i % this.boardSize;
-            Rectangle rectangle = (Rectangle) board.getChildren().get(i);
+            Rectangle rectangle = (Rectangle) node;
             rectangle.setX(squareX(this.boardSize, file, squarePX));
             rectangle.setY(squareY(this.boardSize, rank, squarePX));
             rectangle.setWidth(squarePX);
@@ -75,7 +79,7 @@ public class Main extends Application {
 
             ImageView piece = this.pieces[i];
             if (piece != null) {
-                pane.getChildren().remove(piece);
+                board.getChildren().remove(piece);
                 piece.setX(squareX(this.boardSize, file, squarePX));
                 piece.setY(squareY(this.boardSize, rank, squarePX));
                 piece.setFitWidth(squarePX);
@@ -83,20 +87,21 @@ public class Main extends Application {
                 piece.setOnMouseClicked((event -> {
                     handleClick(i);
                 }));
-                pane.getChildren().add(piece);
-                System.out.println("Got object at " + i);
+                board.getChildren().add(piece);
+                // System.out.println("Got object at " + i);
             }
         }
     }
 
     private void handleClick(int index) {
         if (this.lastClickedIndex == -1) {
-            //start of move source square
+            // start of move source square
             this.lastClickedIndex = index;
-            System.out.println("Storing move: " + index);
+            // System.out.println("Storing move: " + index);
         } else {
-            //We have a finished move to deal with
-            System.out.println("About to make move " + this.lastClickedIndex + " -> " + index);
+            // We have a finished move to deal with
+            // System.out.println("About to make move " + this.lastClickedIndex + " -> " +
+            // index);
             Natives.giveRustMove(this.lastClickedIndex, index);
             this.lastClickedIndex = -1;
         }
@@ -104,10 +109,12 @@ public class Main extends Application {
 
     /**
      * Setups the squares of the board
+     * 
      * @param squarePX
      */
     private void setupBoard(int squarePX) {
         this.board.getChildren().clear();
+
         for (int rank = 0; rank < this.boardSize; rank++) {
             for (int file = 0; file < this.boardSize; file++) {
                 int i = file + rank * this.boardSize;
@@ -126,14 +133,6 @@ public class Main extends Application {
     }
 
     private void setupToolbar() {
-        BorderPane root = new BorderPane();
-        VBox topContainer = new VBox(); // Creates a container to hold all Menu Objects.
-        MenuBar mainMenu = new MenuBar(); // Creates our main menu to hold our Sub-Menus.
-        ToolBar toolBar = new ToolBar(); // Creates our tool-bar to hold the buttons.
-
-        topContainer.getChildren().add(mainMenu);
-        topContainer.getChildren().add(toolBar);
-        root.setTop(topContainer);
 
         // Create and add the "File" sub-menu options.
         Menu file = new Menu("File");
@@ -148,9 +147,10 @@ public class Main extends Application {
         Menu help = new Menu("Help");
         MenuItem visitWebsite = new MenuItem("Visit Website");
         help.getItems().add(visitWebsite);
-        mainMenu.getMenus().addAll(file, edit, help);
+        this.mainMenu.getMenus().addAll(file, edit, help);
 
-        pane.getChildren().add(root);
+        this.root.getChildren().add(this.mainMenu);
+        this.root.setAlignment(Pos.TOP_CENTER);
     }
 
     public Main() {
@@ -169,9 +169,7 @@ public class Main extends Application {
         setupBoard(10);
         setupToolbar();
 
-        pane.getChildren().add(board);
-        pane.getChildren().add(this.pieces[5]);
-        pane.getChildren().add(this.pieces[52]);
+        this.root.getChildren().add(this.board);
     }
 
     public void displayMove(int srcSquare, int destSquare) {
@@ -180,12 +178,13 @@ public class Main extends Application {
         ImageView capturedPiece = this.pieces[destSquare];
         this.pieces[destSquare] = this.pieces[srcSquare];
         this.pieces[srcSquare] = null;
-        System.out.println("moved " + srcSquare + " to " + destSquare);
+        // System.out.println("moved " + srcSquare + " to " + destSquare);
         if (capturedPiece != null) {
-            this.pane.getChildren().remove(capturedPiece);
+            this.board.getChildren().remove(capturedPiece);
         }
 
-        //Refresh the board so that the piece that was just moved is displayed in its new location
+        // Refresh the board so that the piece that was just moved is displayed in its
+        // new location
         resizeBoard(-1.0);
     }
 
@@ -200,11 +199,13 @@ public class Main extends Application {
         width.value = 512;
         height.value = 512;
 
-        stage.setScene(new Scene(pane, width.value, height.value));
+        stage.setScene(new Scene(this.root, width.value, height.value));
         stage.setTitle("Contrasting Chess by Troy Neubauer");
         stage.show();
         this.osTitleBarSize = (int) stage.getHeight() - height.value;
-        System.out.println("Window title bar size is: " + this.osTitleBarSize);
+        this.mainMenuSize = (int) this.mainMenu.getHeight();
+        System.out.println(
+                "Window title bar size is: " + this.osTitleBarSize + " and top border size os: " + this.mainMenuSize);
 
         doResize(width.value, height.value);
 
@@ -222,8 +223,8 @@ public class Main extends Application {
 
     private void doResize(int width, int height) {
         // Always leave one square of padding
-        double squareWidthPX = (double) width / (this.boardSize + 1);
-        double squareHeightPX = (double) (height - this.osTitleBarSize) / (this.boardSize + 1);
+        double squareWidthPX = (double) width / (this.boardSize);
+        double squareHeightPX = (double) (height - this.osTitleBarSize - this.mainMenuSize) / (this.boardSize);
         double squarePX = Math.min(squareWidthPX, squareHeightPX);
 
         resizeBoard(squarePX);
